99c8932d47dded32c78700c3ebc69d071f8d791c
chris <samuelcristian769@gmail.com>
2026-04-08 04:35:18 +0100

    initial commit

---
+++ src/main/java/com/sammy/codexhotel/CodexhotelApplication.java
@@ -0,0 +1,13 @@
+package com.sammy.codexhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexhotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexhotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sammy/codexhotel/CodexhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammy.codexhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexhotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
